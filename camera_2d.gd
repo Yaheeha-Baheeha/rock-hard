8e88ac8e7b42c5019dd8c2f5b5f6b9d78c929a62
Yaheeha-Baheeha <yahalb44@kingshigh.school.nz>
2026-03-05 11:00:33 +1300

    yippeee

--- camera_2d.gd
+++ camera_2d.gd
@@ -5,13 +5,13 @@ extends Camera2D
 func _process(delta):
 	var direction := Vector2.ZERO
 	
-	if Input.is_action_pressed("ui_up") or Input.is_key_pressed(KEY_W):
+	if Input.is_action_pressed("jump") or Input.is_key_pressed(KEY_W):
 		direction.y -= 1
-	if Input.is_action_pressed("ui_down") or Input.is_key_pressed(KEY_S):
+	if Input.is_action_pressed("crouch") or Input.is_key_pressed(KEY_S):
 		direction.y += 1
-	if Input.is_action_pressed("ui_left") or Input.is_key_pressed(KEY_A):
+	if Input.is_action_pressed("left") or Input.is_key_pressed(KEY_A):
 		direction.x -= 1
-	if Input.is_action_pressed("ui_right") or Input.is_key_pressed(KEY_D):
+	if Input.is_action_pressed("right") or Input.is_key_pressed(KEY_D):
 		direction.x += 1
 	
 	if direction != Vector2.ZERO:
