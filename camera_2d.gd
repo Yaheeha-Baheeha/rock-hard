52217d234e58d05c8930c20a3dbc6ad0495a4833
Yaheeha-Baheeha <yahalb44@kingshigh.school.nz>
2026-04-23 09:15:40 +1200

    Add Player.tscn; update camera and level
    Add a new Player.tscn scene implementing a softbody player (SoftBodyController node using softbody_player_controller.gd, SoftBody2D polygon, skeleton look-at modifications, and associated textures/subresources). Also apply minor updates to camera_2d.gd and level_1.tscn to integrate or support the new player scene.

--- camera_2d.gd
+++ camera_2d.gd
@@ -3,9 +3,32 @@ extends Camera2D
 @export var speed: float = 2000.0
 @export var target_path: NodePath
 
-@onready var target: Node2D = get_node_or_null(target_path)
+@onready var target: Node2D = _resolve_target()
+
+func _ready() -> void:
+	if not target:
+		target = _resolve_target()
+
+func _resolve_target() -> Node2D:
+	if not target_path.is_empty():
+		var explicit_target := get_node_or_null(target_path)
+		if explicit_target is Node2D:
+			return explicit_target
+
+	var parent_node := get_parent()
+	if parent_node:
+		var controller := parent_node.get_node_or_null("SoftBodyController")
+		if controller is Node2D:
+			return controller
+		if parent_node is Node2D:
+			return parent_node
+
+	return null
 
 func _process(delta):
+	if not is_instance_valid(target):
+		target = _resolve_target()
+
 	if target:
 		global_position = target.global_position
 		return
