97f859873e4ceb5b1768d2b80c8331f25cd4f48d
Yaheeha-Baheeha <yahalb44@kingshigh.school.nz>
2026-05-06 12:13:10 +1200

    Small stuff
    no more death screen

--- level_select.gd
+++ level_select.gd
@@ -31,7 +31,7 @@ func _configure_level_buttons() -> void:
 
 
 func _on_level_pressed(level: int) -> void:
-	var scene_path := "res://test_level.tscn" if level == 1 else "res://level_%d.tscn" % level
+	var scene_path := "res://level_%d.tscn" % level
 	if ResourceLoader.exists(scene_path):
 		get_tree().change_scene_to_file(scene_path)
 	else:
