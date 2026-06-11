33464e57cafc8b5ae896a1cc20a9e8fc60db7872
Yaheeha-Baheeha <yahalb44@kingshigh.school.nz>
2026-06-11 13:56:45 +1200

    level 6 done but forgot level values

---
+++ tile_map_layer_2.gd
@@ -0,0 +1,15 @@
+extends TileMapLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func player_in():
+	self.modulate = "b7b7b7"
+	print("chud")
